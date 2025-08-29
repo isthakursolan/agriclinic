2be3e12cb7ee386c4fef2652edcbd295854bdd49
isthakursolan <130770005+isthakursolan@users.noreply.github.com>
2025-08-29 14:01:04 +0530

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
     </style>
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
+            {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
         @else
             <style>
                 /*! tailwindcss v4.0.7 | MIT License | https://tailwindcss.com */
